5227b315acb1c6db70724b866d1bb46c2b38b1c5
thdus1323 <thdus1323@naver.com>
2024-07-18 13:49:17 +0900

    하형최고:

--- src/main/java/com/example/emailtest/EmailUtil.java
+++ src/main/java/com/example/emailtest/EmailUtil.java
@@ -2,6 +2,7 @@ package com.example.emailtest;
 
 
 
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -23,7 +24,7 @@ public class EmailUtil {
             helper.setTo(toAddress);
             helper.setSubject(subject);
             helper.setText(body);
-        } catch (jakarta.mail.MessagingException e) {
+        } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
 
